f89760cfefb79e3a4de97d0a4a585b6502f86edd
saad nouioua <saadnouioua12@gmail.com>
2024-09-20 19:23:11 +0100

    update auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,24 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './services/auth.service';
+import { UsersModule } from 'src/users/users.module';
+import { AuthServiceInterface } from './interfaces/auth-service.interface';
 
 @Module({
-  providers: [AuthService]
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
+  providers: [
+    {
+      provide: AuthServiceInterface,
+      useClass: AuthService
+    }
+  ]
 })
 export class AuthModule {}
