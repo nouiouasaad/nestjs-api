7f0279e9b3baf715042df3b942491ec128826c27
saad nouioua <saadnouioua12@gmail.com>
2024-09-20 19:22:57 +0100

    add updatedAt and createdAt to user entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,16 +1,22 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 
-@Entity({name: 'users'})
+@Entity({ name: 'users' })
 export class User {
     @PrimaryGeneratedColumn()
     id: number;
 
-    @Column({type: 'varchar', length: '255'})
+    @Column({ type: 'varchar', length: '255' })
     username: string
 
-    @Column({type: 'varchar', length: '255'})
+    @Column({ type: 'varchar', length: '255' })
     email: string
 
-    @Column({type: 'varchar', length: '255'})
+    @Column({ type: 'varchar', length: '255' })
     password: string
+
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @UpdateDateColumn()
+    updatedAt: Date;
 }
